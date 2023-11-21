e2d7776100a7eb9f492123bdb0daeb58a10c641f
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-11-21 13:03:26 +0200

    refactor(country): streamline and improve country-related components

--- api/src/main/java/com/knits/assetcare/mapper/common/CountryMapper.java
+++ api/src/main/java/com/knits/assetcare/mapper/common/CountryMapper.java
@@ -4,9 +4,18 @@ package com.knits.assetcare.mapper.common;
 import com.knits.assetcare.model.common.Country;
 import com.knits.assetcare.dto.data.common.CountryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface CountryMapper extends EntityMapper<Country,CountryDto> {
 
+    @Named("toCountryDtoDetails")
+    CountryDto toDtoDetails(Country country);
+
+    @Mapping(target = "iso", ignore = true)
+    @Mapping(target = "iso2", ignore = true)
+    @Mapping(target = "iso3", ignore = true)
+    CountryDto toDto(Country country);
 }
 
