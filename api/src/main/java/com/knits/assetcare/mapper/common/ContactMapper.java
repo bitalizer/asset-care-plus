5bef9b73b8fd57f5cb494c147391f05e0f0ce0c2
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-11-21 13:55:40 +0200

    refactor(contact): move ContractMapper to common package

--- api/src/main/java/com/knits/assetcare/mapper/common/ContactMapper.java
+++ api/src/main/java/com/knits/assetcare/mapper/common/ContactMapper.java
@@ -1,15 +1,12 @@
-package com.knits.assetcare.mapper;
+package com.knits.assetcare.mapper.common;
 
 import com.knits.assetcare.dto.data.common.ContactDto;
-import com.knits.assetcare.mapper.common.EntityMapper;
 import com.knits.assetcare.model.common.Contact;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.WARN)
@@ -24,10 +21,5 @@ public interface ContactMapper extends EntityMapper<Contact, ContactDto> {
     @Mapping(source = "note", target = "note", ignore = true)
     ContactDto toDto(Contact contact);
 
-    Contact toEntity(ContactDto contactDto);
-
-    List<ContactDto> toDtos(List<Contact> contacts);
-
-    List<Contact> toEntities(List<ContactDto> contactDtos);
 
 }
