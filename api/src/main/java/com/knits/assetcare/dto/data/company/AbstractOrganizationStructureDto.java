d33bc3ab0e0f2df52c2d51e0b5a24edb4a78199e
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-11-21 13:48:01 +0200

    refactor(company): streamline and improve abstract organization structure components

--- api/src/main/java/com/knits/assetcare/dto/data/company/AbstractOrganizationStructureDto.java
+++ api/src/main/java/com/knits/assetcare/dto/data/company/AbstractOrganizationStructureDto.java
@@ -1,18 +1,18 @@
 package com.knits.assetcare.dto.data.company;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.knits.assetcare.dto.data.common.AbstractAuditableDto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
-@SuperBuilder(toBuilder=true)
+@SuperBuilder(toBuilder = true)
 public class AbstractOrganizationStructureDto extends AbstractAuditableDto {
 
     private String name;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String description;
-    private List<EmployeeDto> employees;
 }
