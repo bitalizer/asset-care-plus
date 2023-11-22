14ac63f7c01ad3d09e4013c1d2165ec680f5d3ce
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-11-22 11:28:08 +0200

    refactor(mapper): implement IgnoreAuditMapping

---
+++ api/src/main/java/com/knits/assetcare/mapper/common/IgnoreAuditMapping.java
@@ -0,0 +1,18 @@
+package com.knits.assetcare.mapper.common;
+
+import org.mapstruct.Mapping;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Mapping(target = "active", ignore = true)
+@Mapping(target = "startDate", ignore = true)
+@Mapping(target = "endDate", ignore = true)
+@Mapping(target = "createdAt", ignore = true)
+@Mapping(target = "updatedAt", ignore = true)
+@Mapping(target = "createdBy", ignore = true)
+public @interface IgnoreAuditMapping {}
---
+++ api/src/main/java/com/knits/assetcare/mapper/inventory/AbstractInventoryItemMapper.java
@@ -0,0 +1,37 @@
+package com.knits.assetcare.mapper.inventory;
+
+import com.knits.assetcare.config.Constants;
+import com.knits.assetcare.dto.data.inventory.AbstractInventoryItemDto;
+import com.knits.assetcare.mapper.common.EntityMapper;
+import com.knits.assetcare.mapper.common.IgnoreAuditMapping;
+import com.knits.assetcare.model.inventory.AbstractInventoryItem;
+import org.mapstruct.*;
+
+@MapperConfig(componentModel = "spring", mappingInheritanceStrategy = MappingInheritanceStrategy.AUTO_INHERIT_ALL_FROM_CONFIG)
+public interface AbstractInventoryItemMapper<E extends AbstractInventoryItem, D extends AbstractInventoryItemDto> extends EntityMapper<E, D> {
+
+    @Named("toDtoAbstractInventoryItem")
+    @IgnoreAuditMapping
+    D toDto(E entity);
+
+    @Mapping(source = "startDate", target = "startDate", dateFormat = Constants.TIME_FORMAT_DD_MM_YYYY_HH_MM_SS)
+    @Mapping(source = "endDate", target = "endDate", dateFormat = Constants.TIME_FORMAT_DD_MM_YYYY_HH_MM_SS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(target = "createdBy", ignore = true)
+    E toEntity(D dto);
+
+    @Named("partialUpdateAbstractInventoryItem")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "startDate", target = "startDate", dateFormat = Constants.TIME_FORMAT_DD_MM_YYYY_HH_MM_SS)
+    @Mapping(source = "endDate", target = "endDate", dateFormat = Constants.TIME_FORMAT_DD_MM_YYYY_HH_MM_SS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(target = "location", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    void partialUpdate(@MappingTarget E entity, D dto);
+
+    @Named("updateAbstractInventoryItem")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(source = "startDate", target = "startDate", dateFormat = Constants.TIME_FORMAT_DD_MM_YYYY_HH_MM_SS)
+    @Mapping(source = "endDate", target = "endDate", dateFormat = Constants.TIME_FORMAT_DD_MM_YYYY_HH_MM_SS, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(target = "location", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    void update(@MappingTarget E entity, D dto);
+}
