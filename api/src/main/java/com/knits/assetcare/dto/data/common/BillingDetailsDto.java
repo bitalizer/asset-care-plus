7276a88afe69e24b28d6d0a7aed992a2d4c97677
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-12-07 16:42:26 +0200

    fix(business): correct inheritance hierarchy for BillingDetailsDto
    The `BillingDetailsDto` was incorrectly extending `AbstractActiveEntity` instead of the appropriate base class `AbstractActiveDto`. This commit corrects the inheritance hierarchy to ensure proper alignment with the intended design.

--- api/src/main/java/com/knits/assetcare/dto/data/common/BillingDetailsDto.java
+++ api/src/main/java/com/knits/assetcare/dto/data/common/BillingDetailsDto.java
@@ -1,7 +1,6 @@
 package com.knits.assetcare.dto.data.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.knits.assetcare.model.common.AbstractActiveEntity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -9,7 +8,7 @@ import lombok.experimental.SuperBuilder;
 @Data
 @NoArgsConstructor
 @SuperBuilder(toBuilder = true)
-public class BillingDetailsDto extends AbstractActiveEntity {
+public class BillingDetailsDto extends AbstractActiveDto {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String shipToName;
