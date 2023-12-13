d8d2fffbcd139c32ba899187f68d21d38fd21726
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-12-13 16:07:55 +0200

    refactor(common): hide active field from basic and detailed dto view

--- api/src/main/java/com/knits/assetcare/mapper/common/AddressMapper.java
+++ api/src/main/java/com/knits/assetcare/mapper/common/AddressMapper.java
@@ -13,8 +13,10 @@ import org.mapstruct.ReportingPolicy;
 public interface AddressMapper extends EntityMapper<Address, AddressDto> {
 
     @Named("toAddressDtoDetails")
+    @Mapping(target = "active", ignore = true)
     AddressDto toDtoDetails(Address address);
 
+    @Mapping(target = "active", ignore = true)
     @Mapping(source = "street", target = "street", ignore = true)
     @Mapping(source = "postalCode", target = "postalCode", ignore = true)
     AddressDto toDto(Address address);
