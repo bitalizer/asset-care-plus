bfab85a2efdf045f0e212e66b9431c627287a2bf
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-12-13 10:42:56 +0200

    refactor(business): refactor CustomerMapper interface and Enhance CustomerService
    CustomerMapper:
    - Renamed method toDtoDetails to toDtoFullDetails for consistency
    - Updated toDtoDetails to include only contact details and ignore billing details
    - Updated toDtoFullDetails to include both contact and billing details
    - Added @IgnoreAuditMapping for improved mapping configuration
    
    CustomerService:
    - Updated saveNewCustomer, findCustomerById, and partialUpdate to return full customer details

--- api/src/main/java/com/knits/assetcare/mapper/business/CustomerMapper.java
+++ api/src/main/java/com/knits/assetcare/mapper/business/CustomerMapper.java
@@ -1,8 +1,10 @@
 package com.knits.assetcare.mapper.business;
 
 import com.knits.assetcare.dto.data.business.CustomerDto;
+import com.knits.assetcare.mapper.common.BillingDetailsMapper;
 import com.knits.assetcare.mapper.common.ContactMapper;
 import com.knits.assetcare.mapper.common.EntityMapper;
+import com.knits.assetcare.mapper.common.IgnoreAuditMapping;
 import com.knits.assetcare.model.business.Customer;
 import org.mapstruct.*;
 import org.springframework.stereotype.Component;
@@ -11,15 +13,20 @@ import java.util.List;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.WARN,
-        uses = {ContactMapper.class})
+        uses = {ContactMapper.class, BillingDetailsMapper.class})
 @Component
 public interface CustomerMapper extends EntityMapper<Customer, CustomerDto> {
 
+    @Named("toCustomerDtoFullDetails")
+    @IgnoreAuditMapping
+    CustomerDto toDtoFullDetails(Customer customer);
+
     @Named("toCustomerDtoDetails")
+    @IgnoreAuditMapping
     @Mapping(target = "billingDetails", ignore = true)
     CustomerDto toDtoDetails(Customer customer);
 
-    @Mapping(target = "active", ignore = true)
+    @IgnoreAuditMapping
     @Mapping(target = "billingDetails", ignore = true)
     @Mapping(target = "type", ignore = true)
     @Mapping(target = "contact", ignore = true)
