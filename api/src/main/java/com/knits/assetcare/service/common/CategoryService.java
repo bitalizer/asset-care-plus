7e17c47b6b10b87d11793c7bf70bb99d733316fe
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2023-12-15 14:04:17 +0200

    feature(controller): streamline CategoryService caching

--- api/src/main/java/com/knits/assetcare/service/common/CategoryService.java
+++ api/src/main/java/com/knits/assetcare/service/common/CategoryService.java
@@ -8,6 +8,7 @@ import com.knits.assetcare.model.common.Category;
 import com.knits.assetcare.repository.common.CategoryRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import java.util.List;
 
 @Service
 @Transactional
+@CacheConfig(cacheNames = "categories")
 @Slf4j
 @AllArgsConstructor
 public class CategoryService {
@@ -29,11 +31,13 @@ public class CategoryService {
         return categoryMapper.toDtoDetails(savedCategory);
     }
 
+    @Cacheable(key = "#id", unless = "#result == null")
     public CategoryDto findCategoryById(Long id) {
         Category category = categoryRepository.findById(id).orElseThrow(() -> new UserException("Category#" + id + " not found"));
         return categoryMapper.toDtoDetails(category);
     }
 
+    @CachePut(key = "#result.id")
     public CategoryDto partialUpdate(CategoryDto categoryDto) {
         Category category = categoryRepository.findById(categoryDto.getId()).orElseThrow(() -> new UserException("Category#" + categoryDto.getId() + " not found"));
 
@@ -42,11 +46,18 @@ public class CategoryService {
         return categoryMapper.toDtoDetails(category);
     }
 
+    @Caching(
+            evict = {
+                    @CacheEvict(key = "#id"),
+                    @CacheEvict(key = "'allCategories'", allEntries = true)
+            }
+    )
     public void deleteCategory(Long id) {
         Category category = categoryRepository.findById(id).orElseThrow(() -> new UserException("Category#" + id + " not found"));
         categoryRepository.delete(category);
     }
 
+    @Cacheable(key = "'allCategories'", unless = "#result.isEmpty()")
     public List<CategoryDto> findAllCategories() {
 
         List<Category> categories = categoryRepository.findByParentCategoryIsNull();
